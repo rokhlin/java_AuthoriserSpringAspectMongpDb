111d8e6868642d4e6e1c7ce746ad3be18464f8e6
rokhlin <rohlinav@gmail.com>
2016-10-24 14:13:43 +0300

    adding comments in AccauntsDao

--- 16_10_23_AuthoriserAspect/16_10_23_AuthorizerAspectMongo/src/tel_ran/mongo/dao/AccauntsDao.java
+++ 16_10_23_AuthoriserAspect/16_10_23_AuthorizerAspectMongo/src/tel_ran/mongo/dao/AccauntsDao.java
@@ -6,10 +6,15 @@ import tel_ran.mongo.entity.Account;
 import tel_ran.mongo.repository.AccountsRepo;
 
 public class AccauntsDao {
+	
 	@Autowired
 	AccountsRepo accountsRepo;
 	
-	
+	/**
+	 * Adding the new user account in MongoDb
+	 * @param acc new account that contain UserName, Password and permission role
+	 * @return false if the UserName already contains in database
+	 */
 	public boolean add(Account acc) {
 		if(acc == null || accountsRepo.exists(acc.getId()))
 			return false;
@@ -19,10 +24,20 @@ public class AccauntsDao {
 		return false;
 	}
 	
+	/**
+	 * 
+	 * @return all user accounts from MongoDb
+	 */
 	public Iterable<Account> getAccounts() {
 		return accountsRepo.findAll();
 	}
 	
+	/**
+	 * 
+	 * @param id unique userName in database  
+	 * @param password
+	 * @return The user role from userName-password pair if userName or password doesn't exist return null
+	 */
 	public String getRole(String id, String password) {
 		accountsRepo.findOne(id);
 		Account account = accountsRepo.findOne(id);
