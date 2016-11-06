ad94fdfe8fcfe37b74c94e2e09e1b83ea3b9b207
rokhlin <rohlinav@gmail.com>
2016-11-06 22:05:55 +0200

    Calculator client

---
+++ 16_11_06_CalculatorClient/src/tel_ran/calculator/api/UrlConstants.java
@@ -0,0 +1,7 @@
+package tel_ran.calculator.api;
+
+public interface UrlConstants {
+	static final String OPERATIONS="operations";
+	static final String GET_CALCULATE ="get_calculate";
+	static final String CALCULATE ="calculate";
+}
